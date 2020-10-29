9b72f93d86a66d6bb26f98afe1b69b0c858b5256
zielapio <piotr.zielinski.cv@gmail.com>
2020-10-29 14:04:00 +0100

    create forms: Mieszkaniec, Wspolnota, Naliczenia

--- create_pdf_documents/forms.py
+++ create_pdf_documents/forms.py
@@ -3,10 +3,90 @@ from django.contrib.auth.forms import UsernameField, ReadOnlyPasswordHashField
 from django.contrib.auth import (authenticate, get_user_model)
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+from django.forms import ModelForm
 from django.utils.text import capfirst
 from django.utils.translation import gettext_lazy as _
+from datetime import datetime
+from .models import Mieszkaniec, Wspolnota, Naliczenie_cala_wspolnota, \
+    Naliczenie_jeden_mieszkaniec
 
 UserModel = get_user_model()
+
+
+class DateInput(forms.DateInput):
+    input_type = "date"
+
+
+class MieszkaniecForm(ModelForm):
+    date = str(datetime.today().strftime("%Y-%m-%d"))
+    aktywny_od = forms.DateField(widget=DateInput, initial=f'{date}', label='Aktywny od:')
+    aktywny_do = forms.DateField(widget=DateInput, initial="2050-01-31", label='Aktywny do:')
+    dane_od = forms.DateField(widget=DateInput, initial=f"{date}", label='Dane ważne od:')
+    dane_do = forms.DateField(widget=DateInput, initial="2050-01-31", label='Dane ważne do:')
+    class Meta:
+        model = Mieszkaniec
+        fields = ['imie', 'nazwisko', 'wspolnota', 'adres_ulica', 'adres_numer_domu',
+                  'adres_numer_mieszkania', 'kod_pocztowy', 'miasto', 'powierzchnia_mieszkania',
+                  'zaliczka_CO', 'zaliczka_CO_miernik', 'podgrzanie_wody_objetosc', 'zimna_woda_objetosc',
+                  'odprowadzenie_sciekow_objetosc', 'uchyl_wody', 'miejsce_postojowe_komorka', 'ilosc_osob', 'winda','aktywny_od', 'aktywny_do',
+                  'dane_od', 'dane_do']
+
+
+class WspolnotaForm(ModelForm):
+    date = str(datetime.today().strftime("%Y-%m-%d"))
+    aktywny_od = forms.DateField(widget=DateInput, initial=f"{date}", label='Aktywna od:')
+    aktywny_do = forms.DateField(widget=DateInput, initial="2050-01-31", label='Aktywna do:')
+    class Meta:
+        model = Wspolnota
+        fields = ['wspolnota','konto_eksploatacyjne','konto_remontowe', 'adres_ulica', 'adres_numer_domu',
+                  'kod_pocztowy', 'miasto', 'aktywny_od', 'aktywny_do']
+
+
+class Naliczenie_cala_wspolnotaForm(forms.ModelForm):
+    date = str(datetime.today().strftime("%Y-%m-%d"))
+    date2 = datetime.today()
+    month, year = date2.month, date2.year
+    if month < 12:
+        data_obowiazywania_oplat = forms.DateField(widget=DateInput, initial=f"{year}-{month+1}-01",label='Początkowa data obowiązywania opłat:')
+    if month == 12:
+        data_obowiazywania_oplat = forms.DateField(widget=DateInput, initial=f"{year+1}-01-01",label='Początkowa data obowiązywania opłat:')
+    data_dokumentu = forms.DateField(widget=DateInput, initial=f"{date}", label='Data wystawienia dokumentu:')
+    oplaty_do = forms.DateField(widget=DateInput, initial="2050-01-31", label='Końcowa data obowiązywania opłat (gdy nieznana: 31.01.2050, pamiętać by ustawić poprawnie gdy jest już znana):')
+
+    class Meta:
+        model = Naliczenie_cala_wspolnota
+        fields = ['data_dokumentu','wspolnota','data_obowiazywania_oplat', 'oplaty_do', 'zmianie_ulegaja_oplaty_za',
+                  'stawka_zaliczka_eksploatacyjna','stawka_CO_miernik', 'stawka_podgrzanie_wody', 'stawka_zimna_woda',
+                  'stawka_odprowadzenie_sciekow', 'stawka_wywoz_nieczystosci', 'stawka_winda', 'stawka_miejsce_postojowe_komorka',
+                  'stawka_fundusz_remontowy', 'dodatkowa_informacja_gora', 'dodatkowa_informacja_dol', 'dodatkowa_informacja_dol_2']
+
+
+class Naliczenie_jeden_mieszkaniecForm(ModelForm):
+    date = str(datetime.today().strftime("%Y-%m-%d"))
+    date2 = datetime.today()
+    month, year = date2.month, date2.year
+    if month < 12:
+        data_obowiazywania_oplat = forms.DateField(widget=DateInput, initial=f"{year}-{month+1}-01",label='Początkowa data obowiązywania opłat:')
+    if month == 12:
+        data_obowiazywania_oplat = forms.DateField(widget=DateInput, initial=f"{year+1}-01-01",label='Początkowa data obowiązywania opłat:')
+    data_dokumentu = forms.DateField(widget=DateInput, initial=f"{date}", label='Data wystawienia dokumentu:')
+    oplaty_do = forms.DateField(widget=DateInput, initial="2050-01-31", label='Końcowa data obowiązywania opłat (gdy nieznana: 31.01.2050, pamiętać by ustawić poprawnie gdy jest już znana):')
+
+    class Meta:
+        model = Naliczenie_jeden_mieszkaniec
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['wlasciciel'].queryset = Mieszkaniec.objects.none()
+        if 'wspolnota' in self.data:
+            try:
+                wspolnota_id = int(self.data.get('wspolnota'))
+                self.fields['wlasciciel'].queryset = Mieszkaniec.objects.filter(wspolnota_id=wspolnota_id).order_by('wspolnota')
+            except (ValueError, TypeError):
+                pass
+        elif self.instance.pk:
+            self.fields['wlasciciel'].queryset = self.instance.wspolnota.mieszkaniec_set.order_by('wspolnota')
 
 
 class AuthenticationForm(forms.Form):
